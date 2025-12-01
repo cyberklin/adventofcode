9c169d909dd3585116351866fef33c8e0ba0661d
klin <n.krapivnyy@corp.badoo.com>
2025-12-01 11:12:47 +0000

    remove debug

--- 2025/day1/1_1.py
+++ 2025/day1/1_1.py
@@ -18,7 +18,6 @@ def main():
     position = 50
     count = 0
     for delta in get_delta():
-        print("position %d, delta %d" % (position, delta))
         position = (position + delta) % 100
         if position == 0:
             count = count + 1
--- 2025/day1/1_2.py
+++ 2025/day1/1_2.py
@@ -26,7 +26,6 @@ def main():
             if position > 0 and (abs(delta) % 100) >= position:
                 zeros = zeros + 1
 
-        print("position %d, delta %d, new_position %d (%d), zeros %d" % (position, delta, new_position, new_position % 100, zeros))
         position = new_position % 100
         passwd = passwd + zeros
 

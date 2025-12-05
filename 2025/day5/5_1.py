cb56180f4376300189a5015c5ab24642cea4d3fd
klin <n.krapivnyy@corp.badoo.com>
2025-12-05 13:10:11 +0000

    add day5

---
+++ 2025/day5/5_1.py
@@ -0,0 +1,53 @@
+import sys
+
+def merge_ranges(ranges):
+
+    merged = True
+    while merged:
+        merged = False
+        for i in range(0, len(ranges) - 1):
+            r1 = ranges[i]
+            for j in range(i + 1, len(ranges)):
+                r2 = ranges[j]
+                separate = r1[1] < r2[0] or r1[0] > r2[1]
+                if not separate: 
+                    merged_range = (min(r1[0], r2[0]), max(r1[1], r2[1]))
+                    ranges = ranges[:i] + ranges[i+1:j] + [merged_range] + ranges[j+1:]
+                    merged = True
+                    break
+            if merged:
+                break
+                
+    return ranges
+
+def read_input(fname):
+ 
+    ranges, ids = [], []
+    with open(fname) as f:
+        for line in f:
+            line = line.rstrip('\n')
+            if not line: 
+                break
+            range = tuple(map(int, line.split('-',2)))
+            ranges.append(range)
+        for line in f:
+            ids.append(int(line.rstrip('\n')))
+            
+    return ranges, ids
+
+def main():
+
+    ranges, ids = read_input(sys.argv[1])
+    ranges = merge_ranges(ranges)
+
+    result = 0
+    for id in ids:
+        for r in ranges:
+            if r[0] <= id <= r[1]: 
+                result += 1 
+                break
+
+    print('result = ', result)
+
+if __name__ == "__main__":
+    main()
